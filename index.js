083af3e783d31fe590a120dbce3a0231e12c930b
Jo Colina <jo.colina@weezevent.com>
2022-03-26 09:40:13 +0100

    Models: add simple model class

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Router = require('./lib/router');
 const App = require('./lib/app');
+const Model = require('./lib/model');
 
-module.exports = { Router, App };
+module.exports = { Router, App, Model };
---
+++ test/utils/kv.js
@@ -0,0 +1,8 @@
+const KV_BINDING = {
+	get: () => Promise.resolve(),
+	put: () => Promise.resolve(),
+	delete: () => Promise.resolve(),
+	list: () => Promise.resolve()
+};
+
+module.exports = { KV_BINDING };
